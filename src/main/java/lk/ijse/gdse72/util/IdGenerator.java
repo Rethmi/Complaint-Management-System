6aff30fb9ceca528fbe4bf7ea36868e9ae3a71c5
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2025-06-16 15:03:07 +0530

    update IDGenerator

--- src/main/java/lk/ijse/gdse72/util/IdGenerator.java
+++ src/main/java/lk/ijse/gdse72/util/IdGenerator.java
@@ -2,15 +2,7 @@ package lk.ijse.gdse72.util;
 
 import java.util.UUID;
 
-//public class IdGenerator {
-//
-//    public static String generateUserId() {
-//        return "USER_" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
-//    }
-//
-//    public static String generateComplaintId() {
-//        return "CMP_" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
-//    }
+
 public class IdGenerator {
     private static int userCounter = 1;
     private static int complaintCounter = 1;
